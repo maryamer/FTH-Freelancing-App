562f5405d96c09a5bb925950b02de92354574c2f
Saheb Mohamadi <saheb.put92@gmail.com>
2023-10-27 12:10:28 +0330

    add initial commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="container xl:max-w-screen-xl">
+      <h1 className="text-3xl bg-red-300">شروع پروژه</h1>
+    </div>
+  );
+}
+
+export default App;
