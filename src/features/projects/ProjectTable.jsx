9dd0696d8e749a1c1f4071df5866239558e722cc
saheb mohammadi <saheb.put92@gmail.com>
2023-11-23 00:49:01 +0330

    project table-confirm delete

--- src/features/projects/ProjectTable.jsx
+++ src/features/projects/ProjectTable.jsx
@@ -1,9 +1,35 @@
-import useProjects from "./useProjects";
+import useOwnerProjects from "./useOwnerProjects";
+import Loading from "../../ui/Loading";
+import Empty from "../../ui/Empty";
+import Table from "../../ui/Table";
+import ProjectRow from "./ProjectRow";
 
 function ProjectTable() {
-  const { data, isPending, error } = useProjects();
-  console.log(data);
+  const { isLoading, projects } = useOwnerProjects();
 
-  return <div>Project Table</div>;
+  if (isLoading) return <Loading />;
+
+  if (!projects.length) return <Empty resourceName="پروژه" />;
+
+  return (
+    <Table>
+      <Table.Header>
+        <th>#</th>
+        <th>عنوان پروژه</th>
+        <th>دسته بندی</th>
+        <th>بودجه</th>
+        <th>ددلاین</th>
+        <th>تگ ها</th>
+        <th>فریلنسر</th>
+        <th>وضعیت</th>
+        <th>عملیات</th>
+      </Table.Header>
+      <Table.Body>
+        {projects.map((project, index) => (
+          <ProjectRow key={project._id} project={project} index={index} />
+        ))}
+      </Table.Body>
+    </Table>
+  );
 }
 export default ProjectTable;
---
+++ src/services/projectService.js
@@ -0,0 +1,6 @@
+import http from "./httpService";
+
+export function getOwnerProjectsApi() {
+  return http.get("/project/owner-projects")
+  .then(({ data }) => data.data);
+}
